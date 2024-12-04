5faad74917157e6f5e2c6adab08c37b44b55d39b
Fasih Ahmed <fasiha042@gmail.com>
2024-12-04 16:32:50 +0500

    Add files via upload

---
+++ final_vs_const.dart
@@ -0,0 +1,30 @@
+
+
+void main(){
+
+  final name = 'Fasih';
+
+  //name = 'Ahmad';   // can not be assigned again bcz of final keyword
+
+  final int number;
+
+  number = 14;
+
+  //number = 7;
+
+  const age = 22;
+
+  //age = 23;
+
+  final names = ['Fasih', 'Ahmad', 'Areeb', 'Ali'];
+
+  names.add('Mimi');
+
+  //names = ['Ahmed', 'Mujtaba', 'Khan', 'Rehman'];
+
+  const numbers = [1, 2, 3, 4];
+
+  //numbers.add(5);   // value can't be added into const created variables
+
+  print('Hello World!');
+}
