39d6a3fe65cea5b63c2e9116ac6b087b70ca082f
Fasih Ahmed <fasiha042@gmail.com>
2024-12-09 16:38:59 +0500

    Add files via upload

---
+++ conditional_prog.dart
@@ -0,0 +1,38 @@
+void main(){
+
+  var a = 100;
+  var b = 50;
+
+  if(a > 200){
+    //true
+    if(b > 100){
+      //when both are true
+    }
+  } else if(a < 50){
+    //true
+  } else if(a > 80){
+    //true
+  } else{
+    //false
+  }
+
+
+//   if (a > 200 && b > 100) {
+//     print('Block 1');
+//   } else if (a < 50) {
+//     print('Block 2');
+//   } else if (a > 80) {
+//     print('Block 3');
+//   } else {
+//     print('Block 4');
+//   }
+
+if(a > 50 || b < 8){
+  print('Block 1');
+} else{
+  print('Else Block');
+}
+
+
+  print('Hello World!');
+}
